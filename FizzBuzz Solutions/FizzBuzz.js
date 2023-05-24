059347a978f43aeb6acd7d011903e408e650b4c4
Jason Vu <jvu@scu.edu>
2023-05-24 11:28:33 -0700

    Create FizzBuzz.js

---
+++ FizzBuzz Solutions/FizzBuzz.js
@@ -0,0 +1,20 @@
+function fizzbuzz(n) {
+  """Implement fizzbuzz algorithm in js"""
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+function main() {
+  fizzbuzz(100);
+}
+
+main();
