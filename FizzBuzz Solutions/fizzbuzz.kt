ac03a123e00a3f9b25e5190af58330e0bab2d036
Amritpal Singh <114454973+AmritS7@users.noreply.github.com>
2023-05-24 15:12:05 -0400

    fizzbuzz in kotlin 1-100

---
+++ FizzBuzz Solutions/fizzbuzz.kt
@@ -0,0 +1,14 @@
+fun fizzbuzz(n: Int) {
+    for (i in 1..n) {
+        when {
+            i % 3 == 0 && i % 5 == 0 -> println("FizzBuzz")
+            i % 3 == 0 -> println("Fizz")
+            i % 5 == 0 -> println("Buzz")
+            else -> println(i)
+        }
+    }
+}
+
+fun main() {
+    fizzbuzz(100)
+}
