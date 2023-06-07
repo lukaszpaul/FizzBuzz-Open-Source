bdac7b645aba0a9b5fdbb2e3e9db2175e6f7f9d8
= <yeohchongyi2@outlook.com>
2023-06-08 01:06:50 +0800

    Update fizzbuzz.js

--- FizzBuzz Solutions/fizzbuzz.js
+++ FizzBuzz Solutions/fizzbuzz.js
@@ -1,10 +1,10 @@
 const fizzbuzz = (n) => {
     for (let i = 1; i <= n; i++) {
-        if (i % 3 === 0 && i % 5 == 0) {
+        if (i % 3 === 0 && i % 5 === 0) {
             console.log("FizzBuzz");
-        } else if (i % 3 == 0) {
+        } else if (i % 3 === 0) {
             console.log("Fizz");
-        } else if (i % 5 == 0) {
+        } else if (i % 5 === 0) {
             console.log("Buzz");
         } else {
             console.log(i);
